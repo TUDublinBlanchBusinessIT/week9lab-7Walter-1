b348950a373e339ae048208d3d3ab8ee143c3e01
B00145274 Walter O Hara <B00145274@mytudublin.ie>
2024-04-08 16:22:56 +0100

    completed step 1

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
 
 
 Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
 Route::post('/customers/create', 'App\Http\Controllers\CustomerController@create')->name('customers.create'); 
-});
+
+Route::get('/calendar/display', 'App\Http\Controllers\calendarController@display');
+
+Route::get('/calendar/json','App\Http\Controllers\CalendarController@json')->name('calendar.json');
+?>
