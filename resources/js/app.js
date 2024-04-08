0a8c404c075229775516773d79fce3f2aee1fcc0
B00145274 Walter O Hara <B00145274@mytudublin.ie>
2024-04-08 15:35:27 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+require('./bootstrap');
+require('jquery/dist/jquery.js');
+require('bootstrap/dist/js/bootstrap.js');
+require('bootstrap-star-rating/js/star-rating.js');
+global.$ = global.jQuery = require('jquery');
